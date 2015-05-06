94ff560f52c7db167b89777947d64aee5676ba7f
Thao Phung <thaophung@thaos-air.uwyo.edu>
2015-05-06 10:52:06 -0600

    Initial Commit

---
+++ Prediction/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Prediction
+//
+//  Created by Thao Phung on 5/6/15.
+//  Copyright (c) 2015 Thao Phung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
